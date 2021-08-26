80ee5c0b1e42faa010f84218906a04ee6629fa86
BeobSik <tjswl756@naver.com>
2021-08-26 15:55:49 +0900

    feat: add @EnableAdminServer

--- src/main/java/com/example/springbootadmin/SpringBootAdminApplication.java
+++ src/main/java/com/example/springbootadmin/SpringBootAdminApplication.java
@@ -1,9 +1,11 @@
 package com.example.springbootadmin;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAdminServer
 public class SpringBootAdminApplication {
 
     public static void main(String[] args) {
